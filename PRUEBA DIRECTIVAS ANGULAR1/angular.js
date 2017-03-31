b39868eaa5c9c1723f36dc1a15c7812af2850ee9
alvera@personalsoft.com.co <alvera@personalsoft.com.co>
2017-03-31 08:57:25 -0500

    se agregan consumo de webserver

--- PRUEBA DIRECTIVAS ANGULAR1/angular.js
+++ PRUEBA DIRECTIVAS ANGULAR1/angular.js
@@ -7,20 +7,20 @@ app.directive("prueba", function(){
   };
 });
 
-app.controller("myController", function($scope, $http, beerService){
+app.controller("myController", function($scope, $http){
   $scope.beers = [];
 
-  $scope.getBeers =  function (){
-    beerService.getBeers().then(function(response){
-      $scope.beers =  response.data;
-        localStorage.setItem("beers",JSON.stringify($scope.beers)); // guarda datos en session
-    })
-  }
-  if(localStorage.hasOwnProperty("beers") ){
-    $scope.beers = JSON.parse(localStorage.getItem("beers"));
-  }else{
-    $scope.getBeers();//ejecturo la funcion
-  }
+
+    $http.get("https://api.punkapi.com/v2/beers").then(function(response){
+        $scope.beers =  response.data;
+        //  localStorage.setItem("beers",JSON.stringify($scope.beers)); // guarda datos en session
+      })
+
+      $scope.buscar= function(){
+          $scope.beers = $scope.beers.filter(function(elem){
+            return elem.id == Number($scope.escriba);
+          })
+        }
 
   $scope.escriba;
 });
@@ -49,5 +49,6 @@ app.factory("beerService", function($http){
   function getBeers(){
     return $http.get("https://api.punkapi.com/v2/beers")
   }
-  
+
 })
+as
