a6423966899f439146eab40aec71f0c4c91088dc
vera <vera975@gmail.com>
2017-03-30 21:02:15 -0500

    no message

--- PRUEBA DIRECTIVAS ANGULAR1/angular.js
+++ PRUEBA DIRECTIVAS ANGULAR1/angular.js
@@ -48,5 +48,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
 app.factory("beerService", function($http){
   function getBeers(){
     return $http.get("https://api.punkapi.com/v2/beers")
+
+    
   }
 })
